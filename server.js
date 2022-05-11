216a5a2b85fa51714e4b61fd4d36be8f930a9e3d
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-05-10 20:49:19 -0700

    adding functions for start up answers

--- server.js
+++ server.js
@@ -31,24 +31,95 @@ function startPrompt() {
     message: "What would you like to do?",
     name: "choice",
     choices: [
-              "View All Employees?", 
-              "View All Employee's By Roles?",
-              "View all Emplyees By Deparments", 
-              "Update Employee",
-              "Add Employee?",
-              "Add Role?",
-              "Add Department?"
+              "View All Employees", 
+              "View All Roles",
+              "View all Deparments", 
+              "View all employees by Department",
+              "Update Employee Role",
+              "Add Employee",
+              "Add Role",
+              "Add Department",
+              "Remove employee",
+              "Remove department",
+              "Remove role",
+              "Exit"
             ]
     }
 ]).then(function(val) {
-    if (answers.choice === "View All Employees?") {
+    if (answers.choice === "View All Employees") {
         console.log(answers.choice)
-    }else {
-        console.log("other")
+        viewEmployees();
+
+    } 
+    if(answers.choice === "View All Roles") {
+        console.log(answers.choice)
+        viewEmployeesByRole();
     }
+    if(answers.choice === "View all employees by Department") {
+        console.log(answers.choice)
+        viewEmployeesByDepartment();
+    }
+    if(answers.choice ===  "View all Deparments"){
+        console.log(answers.choice)
+        viewAllDepartment();
+    }
+    if (answers.choice === "Update Employee Role") {
+        console.log(answers.choice)
+        updateEmployeeRole();
+    }
+    if(answers.choice === "Add Employee") {
+        console.log(answers.choice)
+        addEmployee();
+    }
+    if(answers.choice === "Add Role") {
+        console.log(answers.choice)
+        addRole();
+    }
+    if (answers.choice === "Add Department") {
+        console.log(answers.choice)
+        addDepartment();
+    }
+    if (answers.choice ===  "Remove employee") {
+        console.log(answers.choice)
+        removeEmployee();
+    }
+    if (answers.choice ===  "Remove department") {
+        console.log(answers.choice)
+        removeDepartment();
+    }
+    if (answers.choice ===  "Remove role") {
+        console.log(answers.choice)
+        removeRole();
+    } 
+    if (answers.choice === "Exit")
+        connection.end();
 
     })
 }
+
+
+function viewEmployees() {}
+
+function viewEmployeesByRole() {}
+
+function viewEmployeesByDepartment(){}
+
+function viewAllDepartment(){}
+
+function updateEmployeeRole(){}
+
+function addEmployee(){}
+
+function addRole(){}
+
+function addDepartment(){}
+
+function removeEmployee(){}
+
+function removeDepartment(){}
+
+function removeRole(){}
+
 
 
 
